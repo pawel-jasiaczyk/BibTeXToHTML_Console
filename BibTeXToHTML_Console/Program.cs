5a93144563a2c3ac915c690d84c981b533475df2
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2016-01-06 20:12:26 +0100

    First Commit

---
+++ BibTeXToHTML_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BibTeXtoHTML;
+using BibTeXtoHTML.Project;
+using BibTeXtoHTML.Style;
+using BibTeX;
+using BibTeX.Translator;
+using BibToHtml;
+using BibToHtml.Converter.Styles;
+
+namespace BibTeXToHTML_Console
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Eksperymenty ();
+        }
+
+        static void Eksperymenty ()
+        {
+            Console.WriteLine ("Eksperymenty");
+            ConversionProject project = new ConversionProject ("./Test");
+
+            project.OpenFile ("wos-mariusz.bib");
+            project.ProceedBibTeXFiles ();
+            project.GetHtml ();
+            project.SaveHtml ();
+        }
+    }
+}
