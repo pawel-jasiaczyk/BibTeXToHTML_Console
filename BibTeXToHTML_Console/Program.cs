b26b7c8bc6ab74273162a114acf0d2d0f6465161
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2016-01-08 05:25:06 +0100

    Działa testowa konsolowa aplikacja na starych zasadach

--- BibTeXToHTML_Console/Program.cs
+++ BibTeXToHTML_Console/Program.cs
@@ -13,14 +13,81 @@ namespace BibTeXToHTML_Console
     {
         public static void Main (string[] args)
         {
-            Eksperymenty ();
+            args = FirstConsoleApp ();
+        }
+
+        static string[] FirstConsoleApp ()
+        {
+            string[] args;
+            args = new string[] {
+                "-a",
+                "wos-mariusz.bib",
+                "-o",
+                "test.html"
+            };
+            // Eksperymenty ();
+            bool fail = false;
+            ConversionProject project = new ConversionProject ();
+            for (int i = 0; i < args.Length; i++) {
+                if (fail)
+                    break;
+                if (args [i].StartsWith ("-")) {
+                    switch (args [i]) {
+                    case "-a": {
+                        i++;
+                        for (; i < args.Length; i++) {
+                            if (args [i].StartsWith ("-")) {
+                                i--;
+                                break;
+                            }
+                            try {
+                                project.OpenFile (args [i]);
+                            }
+                            catch (Exception ex) {
+                                Console.WriteLine (ex.Message);
+                            }
+                        }
+                        break;
+                    }
+                    case "-o": {
+                        i++;
+                        if (i < args.Length) {
+                            project.OutputFileName = args [i];
+                            i++;
+                        }
+                        break;
+                    }
+                    case "-s": {
+                        break;
+                    }
+                    default: {
+                        Console.WriteLine ("Nieznane polecenie {0}", args [i]);
+                        fail = true;
+                        break;
+                    }
+                    }
+                }
+                else {
+                    Console.WriteLine ("Nieznane polecenie {0}", args [i]);
+                    break;
+                }
+            }
+            if (project.OutputFileName != null) {
+                project.ProceedBibTeXFiles ();
+                project.GetHtml ();
+                project.SaveHtml ();
+            }
+            else {
+                Console.WriteLine ("Musisz podać nazwę pliku wyjśiowego");
+            }
+            return args;
         }
 
         static void Eksperymenty ()
         {
             Console.WriteLine ("Eksperymenty");
-            ConversionProject project = new ConversionProject ("./Test");
-
+            ConversionProject project = new ConversionProject ();
+            project.OutputFileName = "result.html";
             project.OpenFile ("wos-mariusz.bib");
             project.ProceedBibTeXFiles ();
             project.GetHtml ();
