85d8282a8d45d6c7ee8c4ff79ad84df380286269
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2016-01-20 00:43:26 +0100

    Nowe testy, konfiguracja ułamków HTML OK

--- BibTeXToHTML_Console/Program.cs
+++ BibTeXToHTML_Console/Program.cs
@@ -20,7 +20,38 @@ namespace BibTeXToHTML_Console
                 "test.html",
                 "-p"
             };
-            FirstConsoleApp (args);
+            // FirstConsoleApp (args);
+            CloneTest();
+        }
+
+        static void CloneTest()
+        {
+            LaTeXFunction_MathP p = new LaTeXFunction_MathP ();
+            p.Name = "p";
+            p.DefaultResponse = "p";
+            LaTeXFunction_MathP.OneParameterResponse n = new LaTeXFunction_MathP.OneParameterResponse ();
+            n.Input = "test";
+            n.Response = "wynik";
+            p.Responses.Add (n);
+            try
+            {
+                Console.WriteLine ("{0},{1},{2},{3}", 
+                    p.Name, p.DefaultResponse, p.Responses[0].Input, p.Responses[0].Response);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine (ex.Message);
+            }
+            LaTeXFunction_MathP copy = p.CloneAsType () as LaTeXFunction_MathP;
+            try
+            {
+                Console.WriteLine ("{0},{1},{2},{3}", 
+                    copy.Name, copy.DefaultResponse, copy.Responses[0].Input, copy.Responses[0].Response);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine (ex.Message);
+            }
         }
 
         static void FirstConsoleApp (string[] args)
@@ -30,6 +61,7 @@ namespace BibTeXToHTML_Console
             bool print = false;
             ConversionProject project = new ConversionProject ();
             SetProject (project);
+            SetTestStyle (project);
 
             for (int i = 0; i < args.Length; i++) {
                 if (fail)
@@ -85,6 +117,7 @@ namespace BibTeXToHTML_Console
                 try
                 {
                     project.ProceedBibTeXFiles ();
+                    project.ExecuteStyle();
                     project.GetHtml ();
                     project.SaveHtml ();
                     if(print)
@@ -98,6 +131,10 @@ namespace BibTeXToHTML_Console
             else {
                 Console.WriteLine ("Musisz podać nazwę pliku wyjśiowego");
             }
+            foreach (Exception ex in project.ExceptoptionsLog.Exceptions) 
+            {
+                Console.WriteLine (ex.Message);
+            }
         }
 
         static void SetProject(ConversionProject project)
@@ -105,6 +142,7 @@ namespace BibTeXToHTML_Console
             AddTestFunctions_N (project.BibTeXDataBase.BibTeXTranslator);
             AddTestFunctions_P (project.BibTeXDataBase.BibTeXTranslator);
             AddTestFunctions_PP (project.BibTeXDataBase.BibTeXTranslator);
+            AddTestMathFunctions_P (project.BibTeXDataBase.BibTeXTranslator);
 
 //            project.BibTeXDataBase.UseLocalTranslator = false;
 //            project.BibToHtmlExporter.Translator = project.BibTeXDataBase.BibTeXTranslator;
@@ -140,6 +178,11 @@ namespace BibTeXToHTML_Console
             translator.LaTeXFunctions.Add (textbraceleft);
             translator.LaTeXFunctions.Add (textbraceright);
             translator.LaTeXFunctions.Add (l);
+
+            LaTeXFunction_MathN cdot = new LaTeXFunction_MathN ();
+            cdot.Name = "cdot";
+            cdot.DefaultResponse = "&middot;";
+            translator.MathFunctions.Add (cdot);
         }
 
         static void AddTestFunctions_P(BibTeX.Translator.BibTeXTranslator translator)
@@ -163,6 +206,46 @@ namespace BibTeXToHTML_Console
             frac.DefaultResponse = "({0})/({1}) <p>";
 
             translator.LaTeXFunctions.Add (frac);
+        }
+
+        static void AddTestMathFunctions_P(BibTeX.Translator.BibTeXTranslator translator)
+        {
+            LaTeXFunction_MathNoSlash sup = new LaTeXFunction_MathNoSlash ();
+            sup.Name = "^";
+            sup.DefaultResponse = "<small><sup><sup>{0}</sup></sup></small>";
+
+            LaTeXFunction_MathNoSlash sub = new LaTeXFunction_MathNoSlash ();
+            sub.Name = "_";
+            sub.DefaultResponse = "<sub><sub>{0}</sub></sub>";
+
+            LaTeXFunction_MathPP frac = new LaTeXFunction_MathPP ();
+            frac.Name = "frac";
+            frac.DefaultResponse = 
+                "<table style=\"display:inline-table; border-collapse: collapse; \">" +
+                    "<tr style=\"border-bottom:1px solid black;\">" +
+                        "<td style=\"text-align:center;\">{0}</td>" +
+                    "</tr>" +
+                    "<tr>" +
+                        "<td style=\"text-align:center;\">{1}</td>" +
+                    "</tr>" +
+                "</table>";
+
+            translator.MathFunctions.Add (sup);
+            translator.MathFunctions.Add (sub);
+            translator.MathFunctions.Add (frac);
+        }
+
+        static void SetTestStyle(ConversionProject project)
+        {
+            FieldStyle test = new FieldStyle ("title");
+            test.Prefix = "W książce: \"";
+            test.Suffix = "\"";
+            test.Tags.Add (SupportedHtmlTags.b);
+            project.BibTeXtoHTML_Style.HtmlExporterStyle.DefaultPositionStyle.FieldStyles.Add (test);
+            project.BibTeXtoHTML_Style.HtmlExporterStyle.DefaultPositionStyle.Separator = ", ";
+            project.BibTeXtoHTML_Style.HtmlExporterStyle.DefaultPositionStyle.ParagraphParameters = 
+                "style=\"display:inline-table;\"";
+            project.BibTeXtoHTML_Style.ForceChanges ();
         }
     }
 }
